1928c72f31eaa6c3e755e8dab74509c46d2a8870
Zain Aamer <ZippeyKeys12@gmail.com>
2023-11-28 20:37:36 -0500

    Handle key deletion

--- tests/indexing/update_dict.py
+++ tests/indexing/update_dict.py
@@ -11,6 +11,13 @@ def safe_2(d: dict):
     return d['a'] + d['b']
 
 
+def safe_3(d: dict):
+    d = {'a': 0}
+    d.update({'b': 1})
+
+    return d['a'] + d['b']
+
+
 def unsafe(d: dict):
     d.update({'a': 0})
 
